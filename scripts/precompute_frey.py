5de8cffacf23016128afbd6be3f9f3dcb8c5ec52
Scott Gigante <scottgigante@gmail.com>
2020-01-21 15:54:33 -0500

    precompute frey

---
+++ scripts/precompute_frey.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append("..")  # noqa
+from blog_tools import data, embed
+
+import pickle
+
+dataset = data.frey()
+results = {}
+for algorithm in embed.__all__:
+    results[algorithm.__name__] = algorithm(dataset.X)
+
+results['color'] = dataset.c
+results['images'] = dataset.X_true
+
+with open("../data/frey.pickle", 'wb') as f:
+    pickle.dump(results, f, pickle.HIGHEST_PROTOCOL)
